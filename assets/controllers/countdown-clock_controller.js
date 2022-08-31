c1763ab742b1cdc90aabeae3c0a86fb8da597fff
Thorsten Weber <thorsten.weber@bcmsolutions.de>
2022-08-31 15:04:11 +0200

    Add moment.js and moment-timer.js
    Hopyfully this will simplify timer handling in the stimulus controller

--- assets/controllers/countdown-clock_controller.js
+++ assets/controllers/countdown-clock_controller.js
@@ -1,13 +1,13 @@
 import { Controller } from '@hotwired/stimulus';
+import moment from 'moment';
+import timer from 'moment-timer';
 
 /*
- * This is an example Stimulus controller!
+ * This is Pomodoro / Toto Tomato Timer - Stimulus controller!
  *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
+ * This will control the timer with the variables provided
+ * by the Symfony backend.
  *
- * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
 
@@ -15,6 +15,10 @@ export default class extends Controller {
         pomodoroInMinutes: {type: Number, default: 1},
         refreshInterval: {type: Number, default: 1000},
         pausedTimer: {type: Number, default: 0},
+        pomodoroCycles: {type: Number, default: 0},
+        pomodoroCyclesToLongBreak: {type: Number, default: 0},
+        pomodoroShortBreak: {type: Number, default: 0},
+        pomodoroLongBreak: {type: Number, default: 0},
     }
 
     static targets = [
@@ -24,26 +28,86 @@ export default class extends Controller {
         'countdownResetButton'
     ];
 
+    // TODO: remove when finished
+    // wording:
+    // duration = time span
+    // interval = length of repetetive duration
+    // pomodoroCountdown actual Countdown
+    // shortBreakCountdown actual Countdown
+    // longBreakCountdown: actual Countdown
+    // pomodoroDuration default = 25min
+    // shortBreakDuration default = 5min
+    // longBreakDuration default = 15min
+    // totalCycles default = 1
+    // cyclesToLongBreak default = 2
+
+    /**
+     * Setup pomodoro duration in seconds
+     * @param pomodoroDuration
+     */
+    setPomodoroDurationTimeInSeconds(pomodoroDuration) {
+        this.duration = pomodoroDuration * 60;
+    }
+
+    /**
+     *
+     * @param pomodoroShortBreak
+     */
+    setPomodoroShortBreakTimeInSeconds(pomodoroShortBreak) {
+        this.shortBreakInSeconds = pomodoroShortBreak * 60;
+    }
+
+    /**
+     *
+     * @param pomodoroLongBreak
+     */
+    setPomodoroLongBreakTimeInSeconds(pomodoroLongBreak) {
+        this.longBreakInSeconds = pomodoroLongBreak * 60;
+    }
+
+    /**
+     * Set(up) all provided values as seconds
+     */
+    setupPomodoroValues() {
+        this.totalCycles = this.pomodoroCyclesValue;
+        this.cyclesToLongBreak = this.pomodoroCyclesToLongBreakValue;
+        this.currentCycle = 0;
+        this.setPomodoroDurationTimeInSeconds(this.pomodoroInMinutesValue);
+        this.setPomodoroShortBreakTimeInSeconds(this.pomodoroShortBreakValue);
+        this.setPomodoroLongBreakTimeInSeconds(this.pomodoroLongBreakValue);
+    }
+
+    setStartTime() {
+        this.setPomodoroDurationTimeInSeconds(this.pomodoroInMinutesValue);
+    }
+
+    /**
+     * Invoked with initial load of the timer component
+     */
     connect() {
-        this.setStartTime();
+        console.log(this.pomodoroCyclesValue, this.pomodoroCyclesToLongBreakValue, this.pomodoroShortBreakValue,  this.pomodoroLongBreakValue );
+        this.setupPomodoroValues();
+        // this.setStartTime();
+        console.log(this.duration, this.shortBreakInSeconds, this.longBreakInSeconds);
         this.setTimerDisplayTime();
         this.toggleControlButtonOff(this.countdownStopButtonTarget);
         this.toggleControlButtonOff(this.countdownResetButtonTarget);
     }
 
+    /**
+     * startAction, triggered by start button
+     */
     start() {
 
         if (0 < this.pausedTimerValue) {
-            this.time = this.pausedTimerValue;
+            this.duration = this.pausedTimerValue;
             this.resetPausedTimerValue();
         }
         else{
             this.setStartTime();
         }
 
-        this.refreshTimer = setInterval(() => {
-            this.updateCountdown();
-        }, this.refreshIntervalValue);
+        this.startTimer();
 
         this.toggleControlButtonOff(this.countdownStartButtonTarget);
         this.toggleControlButtonOn(this.countdownStopButtonTarget);
@@ -51,19 +115,12 @@ export default class extends Controller {
 
     }
 
-    updateCountdown() {
-        if (this.time >= 0) {
-            this.setTimerDisplayTime();
-            this.time--;
-        } else {
-            this.playAlarmSound();
-            this.reset();
-        }
-    }
-
+    /**
+     * stopAction, triggered by stop button
+     */
     stop() {
         this.stopTimer();
-        this.pausedTimerValue = this.time;
+        this.pausedTimerValue = this.duration;
 
         this.toggleControlButtonOn(this.countdownStartButtonTarget);
         this.toggleControlButtonOff(this.countdownStopButtonTarget);
@@ -73,12 +130,9 @@ export default class extends Controller {
         this.disconnect();
     }
 
-    stopTimer() {
-        if (this.refreshTimer) {
-           clearInterval(this.refreshTimer);
-        }
-    }
-
+    /**
+     * resetAction, triggered by reset button
+     */
     reset() {
         this.stopTimer();
         this.resetTimers();
@@ -89,39 +143,42 @@ export default class extends Controller {
         this.toggleControlButtonOff(this.countdownResetButtonTarget);
     }
 
-    setTimerDisplayTime() {
-        const minutes = Math.floor(this.time / 60);
-        let seconds = this.time % 60;
+    startTimer() {
+        this.refreshTimer = moment
+            .duration(this.refreshIntervalValue)
+            .timer(
+                {
+                    loop: true
+                },
+                () => {
+                    this.updateCountdown();
+                }
+            );
+        this.refreshTimer.start();
 
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-
-        this.timerDisplayTarget.innerHTML = `${minutes} : ${seconds}`;
+        // this.refreshTimer = setInterval(() => {
+        //     this.updateCountdown();
+        // }, this.refreshIntervalValue);
     }
 
-    setStartTime() {
-        this.time = this.pomodoroInMinutesValue * 60;
-    }
-
-    toggleControlButton(controlButton) {
-        if (true === controlButton.disabled) {
-            controlButton.disabled = false;
-            return;
+    updateCountdown() {
+        // this.duration will be set initially with setStartTime()
+        if (this.duration >= 0) {
+            this.setTimerDisplayTime();
+            this.duration--;
+        } else {
+            this.stopTimer();
+            this.playAlarmSound();
+            // this.endOfCycle();
         }
-        controlButton.disabled = true;
     }
 
-    toggleControlButtonOn(controlButton) {
-        if (true !== controlButton.disabled) {
-            return;
-        }
-        controlButton.disabled = false;
-    }
+    stopTimer() {
+        if (this.refreshTimer) {
+            this.refreshTimer.stop();
 
-    toggleControlButtonOff(controlButton) {
-        if (true === controlButton.disabled) {
-            return;
+            // clearInterval(this.refreshTimer);
         }
-        controlButton.disabled = true;
     }
 
     resetTimers() {
@@ -133,6 +190,50 @@ export default class extends Controller {
         this.pausedTimerValue = 0;
     }
 
+    /**
+     * Write values to frontend
+     */
+    setTimerDisplayTime() {
+        const minutes = Math.floor(this.duration / 60);
+        let seconds = this.duration % 60;
+
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        this.timerDisplayTarget.innerHTML = `${minutes} : ${seconds}`;
+    }
+
+    toggleControlButton(controlButton) {
+        if (true === controlButton.disabled) {
+            controlButton.disabled = false;
+            return;
+        }
+        controlButton.disabled = true;
+    }
+
+    /**
+     * Enable Button
+     * @param controlButton
+     */
+    toggleControlButtonOn(controlButton) {
+        if (true !== controlButton.disabled) {
+            return;
+        }
+        controlButton.disabled = false;
+    }
+
+    /**
+     * Disable Button
+     * @param controlButton
+     */
+    toggleControlButtonOff(controlButton) {
+        if (true === controlButton.disabled) {
+            return;
+        }
+        controlButton.disabled = true;
+    }
+
+
+    // TODO: get sound per api call
     playAlarmSound() {
         this.alarmSound = new Audio("https://cdn.freesound.org/previews/198/198841_285997-hq.mp3");
         this.alarmSound.play();
